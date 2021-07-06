0a9234771bfc2d56a4807d48dc33dad3dc8ac827
Bruno Gomes <gomes.bruno.ac@gmail.com>
2021-07-06 20:46:30 -0300

    refactor: refactored texts

--- refactor-notes.md
+++ refactor-notes.md
@@ -5,6 +5,11 @@ Resume is to get me a job interview, not a job
 - carrer highlight?
 - rewrite job description into company overview, highlights and bullet point tasks
 
+- finish refactoring azion
+- add link hint to accredidation section
+- fix nav bar margin in print mode
+- go wild on the misc section
+
 - Name
 - Contact info (Country, email, github, linkedin, website)
 - Title? (Software Engineer | BSc. Computer Engineering)
--- src/resume.js
+++ src/resume.js
@@ -32,10 +32,9 @@ Vue.component('accredidation', {
 })
 
 Vue.component('work-exp', {
-    // fix me homie, this was some nasty html
     template: `
-    <li>
-        <div class="d-flex flex-row justify-content-between align-items-center">
+    <div v-bind:class="newpage" v-bind:style="component">
+        <div v-bind:style="header">
             <div>
                 <h4>{{company}}</h4>
                 <h6><b>{{position}}</b></h6>
@@ -43,15 +42,46 @@ Vue.component('work-exp', {
 
             <div> {{startDate}} - {{endDate}} <i class="far fa-calendar-alt"></i> </div>
         </div>
-            <p :style="content" v-html="desc">
-            </p>
-    </li>
+
+        <p v-html="intro"></p>
+
+        <ul v-bind:style="activitiesStyle">
+          <li v-for="activity in activities"> {{activity}} </li>
+        </ul>
+
+    </div>
     `,
-    props: ['company', 'position', 'startDate', 'endDate', 'desc'],
+    props: {
+        newpage: {
+            default: false,
+            type: Boolean
+        },
+        company: String,
+        position: String,
+        startDate: String,
+        endDate: String,
+        intro: String,
+        activities: Array
+    },
     data: function() {
         return {
             content: {
                 marginLeft: '10px',
+            },
+
+            component: {
+                paddingBottom: '28px',
+
+            },
+
+            activitiesStyle: {
+                marginLeft: '18px',
+            },
+
+            header: {
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
             }
         }
     }
@@ -65,12 +95,17 @@ new Vue({
         position: 'Software Engineering Intern',
         startDate: 'Nov/2019',
         endDate: 'Jul/2020',
-        desc: `
-At wipro I worked as a fullstack software engineer. During my stay there I: developed Spring Boot based REST APIs; Single Page Application development with Angular; built CI/CD pipelines - from scratch - for a Dockerized Spring Boot app.
-<br>
-I am specially proud about an initiative proposed by my supervisor.
-I prototyped a system to structure the company's interview process, which later on was widely adopted by the managers to improve their workflow.
-    `
+        intro: `
+            Wipro is a Fortune India 500 company with a large IT service providing department.
+            During my internship I worked as a fullstack software engineer, which significantly strengthened my skills.
+            As a highlight of my time there, I prototyped a system to structure the company's techincal interviewing prodecures.
+            The MVP was refined and standardized by the managers as part of the process.
+        `,
+        activities: [
+            'Full stack development using Spring Boot for REST APIs and Angular front end.',
+            'Built CI and CD pipelines, from scratch, for Dockerized Java Maven projects and Angular projects.',
+            'Django development for an MVC application.',
+        ]
     }
 })
 
@@ -81,18 +116,21 @@ new Vue({
         position: 'Software Developer and DevOps Enabler',
         startDate: 'Oct/2020',
         endDate: 'Jan/2020',
-        desc: `
-I was part of the first development team at Code Fiction, while they were setting up and stablishing processes.
-I embraced the task of doing the initial setup so the team could start working on the MVP.
-As such I setup CI/CD pipelines and configured our environments such that they followed cloud native practices (container orchestration, gitflow, infrastructure as code).
-This initial setup proved invaluable as it guaranteed the application's consistency accross environments and automated processes which empowered the team to focus on the development tasks.
-The infrastructure stack was: Docker, Terraform, Docker Compose, Gitlab CI, Nginx and AWS services.
-<br>
-Although I worked on establishing processes and spreading the DevOps culture, my main role was still of a Software Developer.
-The development tasks consisted of REST APIs enabled by Spring Boot (Spring MVC) and the code base was architected following DDD practices.
-During that time I implemented a Single Sign On feature which allowed users to login using Google and Facebook.
-The authentication flow made use of the OAuth and OpenID Connect standards.
-`
+        intro: `
+        Codefiction is a technology startup company which provides IT services to their clients.
+        While there I worked as a Backend Software Developer, using Spring Boot and helped establish processes.
+        I was part of their first development team and I embraced the task of doing the initial infrastructure setup so that the team could start working on the MVP.
+        As such I setup CI/CD pipelines and configured our environments such that they followed cloud native practices (container orchestration, gitflow, infrastructure as code).
+        This initial setup proved invaluable as it guaranteed consistency among team member and environments.
+        Furthermore, it automated processes which empowered the team to focus on the development tasks.
+        `,
+        activities: [
+            'Architecture definition for backend application using DDD.',
+            'Development of Backend Enterprise Application REST APIs with Spring Boot.',
+            'Using Spring Security, built a Social Authentication scheme leveraging OAuth and OpenID Connect.',
+            'Setup and provisioning of VPS and deployment automation.',
+            'Configured CI and CD pipelines using: Docker, Terraform, Gitlab CI, Nginx and AWS Services.',
+        ]
     }
 })
 
@@ -102,20 +140,24 @@ new Vue({
         company: 'Azion',
         position: 'Software Developer',
         startDate: 'Jan/2021',
-        endDate: '',
-        desc: `
-Azion is one of the leading edge computing providers.
-Their stack is extensive and cutting edge.
-
-At Azion I work with Django to maintain the legacy MVC system and code new features using an API first approach following REST practices.
-Although the main system is written in Django, I work on multiple services, some of which are microservices written in pure Python with an RPC interface.
-I continously look to improve the existing monolithic system, experimenting with different abstractions leveraging Python's more advanced featured (meta programming, context managers, iterators) to improve code readability and reduce repetition.<br>
-I am particularly proud of solution I've worked on where I leveraged in-depth knowledge of Python to fix a security flaw in our application.
-The solution proved capable of patching the vulnerability without requiring refactoring the legacy code.
-Some other tools and languages I use in order to work with our entire stack are: Grafana, Prometheus, Kibana, Puppet, Nginx, Kubernetes, Docker, GitHub Actions and Rust.
-<br>
-At Azion I've seen what it means to be best-in-class and it's been a rich learning experience.
-`
+        endDate: 'Present',
+        intro: `
+        Azion is one of the leading edge computing service providers.
+        Their products are technical in nature such as web-application firewalls, load ballancing and content caching.
+        <br>
+        My primary goals are to improve and add new features to their security products (edge firewall and web application firewal).
+        The stack is primarily composed of Django, RPC Python services and parts of their infrastructure such as Nginx. 
+        Since a fair ammount of the code base is quite old, I continuously look to improve it and create better abstractions to simplify the existing code.
+        <br>
+        I am particularly proud of solution I've worked on where I leveraged in-depth knowledge of Python to fix a security flaw in our application.
+        The solution proved capable of patching the vulnerability without requiring refactoring the legacy code.
+`,
+        activities: [
+            'Improvements and bugfixes for the existing Django MVC application',
+            'Architecting and implementing new endpoints for the security products REST API.',
+            'Proposed several improvements to refactor the old code case.',
+            'Worked with various tools to perform my tasks, such as: Grafana, Puppet, Kibana, Clikchouse, Nginx, Naxsi, Docker, Github Actions',
+        ]
     }
 })
 
